e92f08d6be76a742827ca89b6ba1b86ecb09196e
Srinjoy <srinjoypramanik2004@gmail.com>
2024-07-27 20:43:00 +0530

    subarraysumequalsk

---
+++ Randoms/subarraysumsequalk.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> mpp;
+        mpp.insert({0, 1});
+        int res = 0;
+        int cumsum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            cumsum = cumsum + nums[i];
+            if (mpp.find(cumsum - k) != mpp.end()) {
+                res = res + mpp[cumsum - k];
+            } 
+                mpp[cumsum]++;
+        }
+        return res;
+    }
+};
