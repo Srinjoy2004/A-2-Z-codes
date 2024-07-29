ab34996aff4b46c3a919ec7b5eb57cb98e5e2578
Srinjoy <srinjoypramanik2004@gmail.com>
2024-07-29 08:13:40 +0530

    lambda func

--- Randoms/sortarraybyincreasingfrq.cpp
+++ Randoms/sortarraybyincreasingfrq.cpp
@@ -32,3 +32,19 @@ public:
         return result;
     }
 };
+
+//method 2 using lambda func
+
+class Solution {
+public:
+    vector<int> frequencySort(vector<int>& nums) {
+        unordered_map<int, int> mpp;
+        for (auto it : nums) {
+            mpp[it]++;
+        }
+        sort(nums.begin(), nums.end(), [&](int a, int b) {
+            return mpp[a] != mpp[b] ? mpp[a] < mpp[b] : a > b;
+        });
+        return nums;
+    }
+};
