7d8b03e70878e4d6941b4b2445978acc724c327b
Srinjoy <srinjoypramanik2004@gmail.com>
2024-08-28 11:39:23 +0530

    optimal majority element 2(modified moores voting algo)

--- array/majority_element_II.cpp
+++ array/majority_element_II.cpp
@@ -21,3 +21,59 @@ public:
         return nums;
     }
 };
+
+// optimal
+// modified moores voting algo
+
+class Solution
+{
+public:
+    vector<int> majorityElement(vector<int> &nums)
+    {
+        int first = nums[0];
+        int second = nums[0];
+        int fc = 0;
+        int sc = 0;
+        if (nums.size() < 2)
+            return nums;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (first == nums[i])
+                fc++;
+            else if (second == nums[i])
+                sc++;
+            else if (fc == 0)
+            {
+                first = nums[i];
+                fc = 1;
+            }
+            else if (sc == 0)
+            {
+                second = nums[i];
+                sc = 1;
+            }
+            else
+            {
+                sc--;
+                fc--;
+            }
+        }
+        if (first == second)
+            return {first};
+        fc = 0;
+        sc = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] == first)
+                fc++;
+            if (nums[i] == second)
+                sc++;
+        }
+        vector<int> res;
+        if (fc > floor(nums.size() / 3))
+            res.push_back(first);
+        if (sc > floor(nums.size() / 3))
+            res.push_back(second);
+        return res;
+    }
+};
