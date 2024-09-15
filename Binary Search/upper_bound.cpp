b1080ab52f73d01daece89de7095e83688ce3bef
Srinjoy <srinjoypramanik2004@gmail.com>
2024-09-15 13:28:25 +0530

    upper bound

--- Binary Search/upper_bound.cpp
+++ Binary Search/upper_bound.cpp
@@ -1,1 +1,32 @@
-//https://www.naukri.com/code360/problems/implement-upper-bound_8165383?utm_source=youtube&utm_medium=affiliate&utm_campaign=codestudio_Striver_BinarySeries
+// https://www.naukri.com/code360/problems/implement-upper-bound_8165383?utm_source=youtube&utm_medium=affiliate&utm_campaign=codestudio_Striver_BinarySeries
+
+C++ Java
+    Python
+        JavaScript
+            Copy
+
+#include <bits/stdc++.h>
+    using namespace std;
+
+int upperBound(vector<int> &arr, int x, int n)
+{
+    int low = 0, high = n - 1;
+    int ans = n;
+
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        // maybe an answer
+        if (arr[mid] > x)
+        {
+            ans = mid;
+            // look for smaller index on the left
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1; // look on the right
+        }
+    }
+    return ans;
+}
