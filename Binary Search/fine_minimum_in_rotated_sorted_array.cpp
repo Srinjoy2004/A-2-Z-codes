6aebe52d824a6f848011c62298f24a33d3c71d37
Srinjoy <srinjoypramanik2004@gmail.com>
2024-09-23 10:17:52 +0530

    single element in a sorted array

--- Binary Search/fine_minimum_in_rotated_sorted_array.cpp
+++ Binary Search/fine_minimum_in_rotated_sorted_array.cpp
@@ -1,22 +1,27 @@
-//https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/description/
+// https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/description/
 
-class Solution {
+class Solution
+{
 public:
-    int findMin(vector<int>& nums) {
+    int findMin(vector<int> &nums)
+    {
         int n = nums.size();
         int low = 0;
         int high = n - 1;
         int mini = INT_MAX;
 
-        while (high >= low) {
+        while (high >= low)
+        {
             int mid = (low + high) / 2;
 
             // checking whether left part is sorted or not
-            if (nums[low] <= nums[mid]) {
+            if (nums[low] <= nums[mid])
+            {
                 mini = min(mini, nums[low]);
                 low = mid + 1;
-
-            } else if (nums[mid] <= nums[high]) {
+            }
+            else if (nums[mid] <= nums[high])
+            {
                 mini = min(mini, nums[mid]);
                 high = mid - 1;
             }
