146e1812405837396183f6dc448b5653fb48094d
Srinjoy <srinjoypramanik2004@gmail.com>
2024-09-24 12:42:28 +0530

    peak element

---
+++ Binary Search/peak_element.cpp
@@ -0,0 +1,16 @@
+//https://leetcode.com/problems/find-peak-element/
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& arr) {
+        int n = arr.size(); // Size of array.
+
+        for (int i = 0; i < n; i++) {
+            // Checking for the peak:
+            if ((i == 0 || arr[i - 1] < arr[i]) &&
+                (i == n - 1 || arr[i] > arr[i + 1])) {
+                return i;
+            }
+        }return -1;
+    }
+};
