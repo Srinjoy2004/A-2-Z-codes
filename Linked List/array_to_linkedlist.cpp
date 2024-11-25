2564cc86415e6d6b46321ef705b110308a220fe2
Srinjoy <srinjoypramanik2004@gmail.com>
2024-11-25 19:47:16 +0530

    length of a linked list

--- Linked List/array_to_linkedlist.cpp
+++ Linked List/array_to_linkedlist.cpp
@@ -3,54 +3,71 @@
 
 using namespace std;
 
-class Node {
+class Node
+{
 public:
     int data;
-    Node* next;
+    Node *next;
 
     // Constructor for creating a node with data and next pointer
-    Node(int data1, Node* next1) {
+    Node(int data1, Node *next1)
+    {
         data = data1;
         next = next1;
     }
 
     // Constructor for creating a node with data only
-    Node(int data1) {
+    Node(int data1)
+    {
         data = data1;
         next = nullptr;
     }
 };
 
 // Function to convert an array to a linked list
-Node* convertarr2LL(vector<int>& arr) {
-    if (arr.empty()) return nullptr; // Handle empty array case
+Node *convertarr2LL(vector<int> &arr)
+{
+    if (arr.empty())
+        return nullptr; // Handle empty array case
 
-    Node* head = new Node(arr[0]); // Create the head node
-    Node* mover = head; // Pointer pointing towards head
+    Node *head = new Node(arr[0]); // Create the head node
+    Node *mover = head;            // Pointer pointing towards head
 
-    for (int i = 1; i < arr.size(); i++) {
-        Node* temp = new Node(arr[i]); // Create a new node
-        mover->next = temp; // Link the current node to the new node
-        mover = mover->next; // Move to the new node
+    for (int i = 1; i < arr.size(); i++)
+    {
+        Node *temp = new Node(arr[i]); // Create a new node
+        mover->next = temp;            // Link the current node to the new node
+        mover = mover->next;           // Move to the new node
     }
 
     return head;
 }
 
 // Function to print the linked list
-//this is basically linked list traversal
-void printLinkedList(Node* head) {
-    Node* current = head;
-    while (current != nullptr) {
+// this is basically linked list traversal
+// length of linked list
+void printLinkedList(Node *head)
+{
+    Node *current = head;
+    int count = 0;
+    if (current != nullptr)
+        count++;
+    while (current != nullptr)
+    {
         cout << current->data << " ";
+        // cout<<endl;
         current = current->next;
+        if (current != nullptr)
+            count++;
     }
     cout << endl;
+    cout << count << " ";
 }
 
-int main() {
+int main()
+{
     vector<int> arr = {2, 5, 8, 7};
-    Node* head = convertarr2LL(arr);
+    Node *head = convertarr2LL(arr);
 
     // Print the linked list to verify
     cout << "Linked list: ";
