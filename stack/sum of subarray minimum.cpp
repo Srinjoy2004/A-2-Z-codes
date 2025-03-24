5d2b5ca42164613850d0a60c82cc03c82e55b8c9
Srinjoy <srinjoypramanik2004@gmail.com>
2025-03-24 12:03:38 +0530

    sum of subarray minimum

---
+++ stack/sum of subarray minimum.cpp
@@ -0,0 +1,72 @@
+//https://leetcode.com/problems/sum-of-subarray-minimums/
+
+class Solution {
+    public:
+        vector<int> getNSL(vector<int>& arr, int n) {
+            vector<int> result(n);
+            stack<int> st;
+    
+            for (int i = 0; i < n; i++) {
+                if (st.empty()) {
+                    result[i] = -1;
+                } else {
+                    while (!st.empty() && arr[st.top()] > arr[i]) {
+                        st.pop();
+                    }
+    
+                    if (st.empty()) {
+                        result[i] = -1;
+                    } else {
+                        result[i] = st.top();
+                    }
+                }
+                st.push(i);
+            }
+    
+            return result;
+        }
+    
+        vector<int> getNSR(vector<int>& arr, int n) {
+            vector<int> result(n);
+            stack<int> st;
+    
+            for (int i = n - 1; i >= 0; i--) {
+                if (st.empty()) {
+                    result[i] = n;
+                } else {
+                    while (!st.empty() && arr[st.top()] >= arr[i]) {
+                        st.pop();
+                    }
+    
+                    if (st.empty()) {
+                        result[i] = n;
+                    } else {
+                        result[i] = st.top();
+                    }
+                }
+                st.push(i);
+            }
+    
+            return result;
+        }
+        int sumSubarrayMins(vector<int>& arr) {
+            int n = arr.size();
+            vector<int> NSL = getNSL(arr, n);
+            vector<int> NSR = getNSR(arr, n);
+    
+            long long sum = 0;
+            int M = 1e9 + 7;
+    
+            for (int i = 0; i < n; i++) {
+                long long ls = i - NSL[i];
+                long long rs = NSR[i] - i;
+    
+                long long totalways = ls * rs;
+    
+                long long totalsum = arr[i] * totalways;
+    
+                sum = (sum + totalsum) % M;
+            }
+            return sum;
+        }
+    };
