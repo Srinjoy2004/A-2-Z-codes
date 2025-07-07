406f088b8f192e52baf512cb98b650d00e91be1d
Srinjoy <srinjoypramanik2004@gmail.com>
2025-07-07 23:02:03 +0530

    no of nodes

---
+++ binary tree/CountNoOfNodes.cpp
@@ -0,0 +1,54 @@
+//https://leetcode.com/problems/count-complete-tree-nodes/
+
+//method 1
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void total(TreeNode* root, int &count)
+ {
+        if (root ==NULL)
+            return;
+        count++;
+        total(root->left, count);
+        total(root->right, count);
+    }
+    int countNodes(TreeNode* root) {
+        int count = 0;
+        total(root, count);
+        return count;
+    }
+};
+
+//method 2
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+       if(root==NULL)
+       return 0;
+       return (1+countNodes(root->left)+countNodes(root->right)) ;
+    }
+};
