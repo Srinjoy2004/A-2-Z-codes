3b047d02401d25b40cfdb6e1b58d23e7d0236b80
Srinjoy Pramanik <srinjoypramanik2004@gmail.com>
2026-09-23 20:58:04 +0530

    LeetCode 20: Add Valid Parentheses using stack

---
+++ stack/20_ValidParentheses.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for (int i = 0; i < s.length(); i++) {
+
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[')
+                st.push(s[i]);
+            else if (st.empty() == false) {
+                if (s[i] == ')' && st.top() == '(')
+                    st.pop();
+                else if (s[i] == '}' && st.top() == '{')
+                    st.pop();
+                else if (s[i] == ']' && st.top() == '[')
+                    st.pop();
+                else
+                    st.push(s[i]);
+            } else
+                st.push(s[i]);
+        }
+
+        if (st.empty() == true)
+            return true;
+        else
+            return false;
+    }
+};
